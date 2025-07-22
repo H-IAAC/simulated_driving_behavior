444dc18e0c37f77c5880516a22984c28de4de1cc
RenanFlorencio <renanm.florencio@gmail.com>
2025-07-22 15:45:45 -0300

    reorganizing merged data and writing training code

--- src/data/preprocessor.py
+++ src/data/preprocessor.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def sliding_windows(data, label, window_size=7, step_size=1):
+def sliding_windows(data, window_size=7, step_size=1):
     # This function returns the sliding windows and the labels for each window
     # If output_3d is True, the output will be a 3D array, otherwise it will be a 2D array
     # The 2D array is (num_samples, num_features), useful for ML and the 3D array is (num_samples, window_size/time_steps, num_features), useful for RNNs
@@ -22,6 +22,12 @@ def sliding_windows(data, label, window_size=7, step_size=1):
                 (window_size - len(window), len(window.columns)))
             # Stacking the window and padding keeping the same order
             window = np.vstack([window, padding])
+
+        # Get the label from the first row of the window
+        label = window.iloc[0]['label'] if 'label' in window.columns else ValueError(
+            "Data must contain a 'label' column for classification.")
+        window = window.drop(
+            columns=['label'])
 
         X.append(np.array(window).flatten())
         y.append(label)
